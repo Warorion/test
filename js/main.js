7831bf860d30cc06885260d391e49a0d9445d8d8
unknown <orion.number1@yandex.ru>
2020-11-04 23:27:02 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.getElementById('burger').onclick = function () {    document.getElementById('menu').classList.add('open');};document.querySelectorAll('#menu > *').forEach((item) => {    item.onclick = () => {        document.getElementById('menu').classList.remove('open');    }})
